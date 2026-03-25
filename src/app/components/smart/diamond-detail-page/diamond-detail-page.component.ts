57c1955787d832a145b6fc582e281e95f0809945
Aurore  <hoshizora972@gmail.com>
2026-03-25 18:32:51 -0400

    add navigation on the details page

--- src/app/components/smart/diamond-detail-page/diamond-detail-page.component.ts
+++ src/app/components/smart/diamond-detail-page/diamond-detail-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
+import { DiamondService } from '../../../services/diamond.service';
 
 @Component({
   selector: 'app-diamond-detail-page',
@@ -7,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './diamond-detail-page.component.scss'
 })
 export class DiamondDetailPageComponent {
+  
+  // injection des données du service en privé
+  private diamondService = inject(DiamondService);
 
+  // mise en place d'un input signal qui va recevoir la valeur de diamsID depuis L'URL
+  diamsID = input.required<string>();
+
+  // création d'un signal dérivé (computed) qui cherche le diamant dès que l'id change
+  diamond = computed (()=>
+    this.diamondService.getDiamondById(this.diamsID())
+  );
+
+  // mise en place d'une fonction pour retourner en arrière
+  goBack(){
+    window.history.back();
+  }
 }
